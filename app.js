061b4bd2574d1ab68ef7e7b43190cba421f69c6c
emmaneely <emmaneely3@gmail.com>
2022-06-08 11:39:44 -0500

    Completed exercise 3

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ if (number < favNumber) {
 } else if (number > favNumber) {
   console.log("too high");
 }
+  else if (number == favNumber) {
+    console.log("Congratulations!!!")
+}
   else {
-    console.log ("Congratulations!!!")
-  }
+    console.log ("That is not a valid number.")
+}
 
   // Exercise 2
   let birthMonth = window.prompt("What is your birth month?");
@@ -44,3 +47,65 @@ if (number < favNumber) {
     default:
       console.log("That is not a valid month.")
   }
+
+  // Exercise 3
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+switch (typeId) {
+  case "01":
+    type = "Tank top"
+    break;
+  case "02":
+    type = "T-shirt"
+    break;
+  case "03":
+    type = "Long Sleeve"
+    break;
+  case "04":
+    type = "Sweat Shirt"
+    break;
+  default:
+    type = "Other"
+}
+
+switch (colorId) {
+  case "BK":
+    color = "Black"
+    break;
+  case "BL":
+    color = "Blue"
+    break;
+  case "RD":
+    color = "Red"
+    break;
+  case "PU":
+    color = "Purple"
+    break;
+  default:
+    color = "White"
+}
+
+switch (sizeId) {
+  case "S":
+    size = "Small"
+    break;
+  case "M":
+    size = "Medium"
+    break;
+  case "L":
+    size = "Large"
+    break;
+  case "XL":
+    size = "Extra Large"
+    break;
+  default:
+    size = "One Size Fits All"
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
