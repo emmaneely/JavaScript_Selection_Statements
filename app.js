b9d40b2ba07eec186e6d9991156a95d1f9ec7174
emmaneely <emmaneely3@gmail.com>
2022-06-08 11:05:32 -0500

    Completed exercise 2

--- app.js
+++ app.js
@@ -16,3 +16,31 @@ if (number < favNumber) {
   else {
     console.log ("Congratulations!!!")
   }
+
+  // Exercise 2
+  let birthMonth = window.prompt("What is your birth month?");
+
+  switch (birthMonth) {
+    case "December":
+    case "January":
+    case "February":
+      console.log("Winter")
+      break;
+    case "March":
+    case "April":
+    case "May":
+      console.log("Spring")
+      break;
+    case "June":
+    case "July":
+    case "August":
+      console.log("Summer")
+      break;
+    case "September":
+    case "October":
+    case "November":
+      console.log("Fall")
+      break;
+    default:
+      console.log("That is not a valid month.")
+  }
