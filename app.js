da85438670a46a2167cebc172def190c4bd198fa
emmaneely <emmaneely3@gmail.com>
2022-06-08 10:47:18 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+
+// Exercise 1
+var favNumber = 33;
+
+let number = window.prompt("Please enter a number.");
+
+if (number < favNumber) {
+  console.log("too low");
+} else if (number > favNumber) {
+  console.log("too high");
+}
+  else {
+    console.log ("Congratulations!!!")
+  }
